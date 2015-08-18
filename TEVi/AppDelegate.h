20afc4ca199a07a2aa873c8ec7fcee79d3af7378
angelrsolsonabosc <angelrsolsona@iMac-de-Solsona.local>
2015-08-18 09:26:51 -0500

    Initial Commit

---
+++ TEVi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEVi
+//
+//  Created by Angel  Solsona on 18/08/15.
+//  Copyright (c) 2015 Kelevrads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEVi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEVi
+//
+//  Created by Angel  Solsona on 18/08/15.
+//  Copyright (c) 2015 Kelevrads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
